09a4db99c320e409c12e6f501768d34c759ad6b0
powcoder <powcoder@163.com>
2022-12-07 10:51:15 +0800

    CS4410 Operating Systems a4

---
+++ block_store.c
@@ -0,0 +1,24 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/*
+ * (C) 2017, Cornell University
+ * All rights reserved.
+ */
+
+/* Author: Robbert van Renesse, August 2015
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "block_store.h"
+
+void panic(const char *s){
+	fprintf(stderr, "!!PANIC: %s\n", s);
+	exit(1);
+}
---
+++ sample.c
@@ -0,0 +1,28 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/*
+ * (C) 2017, Cornell University
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include "disk.c"
+
+int main(){
+	block_store_t *disk = disk_init("disk.dev", 1024);
+	block_t block;
+	strcpy(block.bytes, "Hello World");
+	(*disk->write)(disk, 0, &block);
+	(*disk->destroy)(disk);
+	return 0;
+}
